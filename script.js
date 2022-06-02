446635bfa85a6eb232f797a52122dd4baed12e77
Lucas Carvalho <lucascarvalhopc@gmail.com>
2022-06-02 15:34:20 -0300

    added accordion list and desktop version

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('ola');
+const titles = document.querySelectorAll('.questions dt');
+const firstTitle = titles[0];
+firstTitle.classList.add('active');
+firstTitle.nextElementSibling.classList.add('active')
+
+function showInfo() {
+  this.classList.toggle('active')
+  this.nextElementSibling.classList.toggle('active');
+}
+
+titles.forEach((title) => {
+  title.addEventListener('click', showInfo)
+})
+
